7c2fd1fdcb35cc0722f59d333722307249012c76
zpxttz <bjszpx@126.com>
2017-03-30 10:03:11 +0800

    Initial Commit

---
+++ ZPXQRCodeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZPXQRCodeDemo
+//
+//  Created by zpx on 2017/3/30.
+//  Copyright © 2017年 zpx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZPXQRCodeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZPXQRCodeDemo
+//
+//  Created by zpx on 2017/3/30.
+//  Copyright © 2017年 zpx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
